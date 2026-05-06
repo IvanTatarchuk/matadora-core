649fe40b6f9deb0d7e18b95bb0c62ee72eb0b45a
IvanTatarchuk <itatarchuk1202@gmail.com>
2026-05-06 09:54:38 +0200

    debug: surface backend import error

--- api/index.py
+++ api/index.py
@@ -1,6 +1,20 @@
 import sys
 import os
+import traceback
 
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from backend.api.main import app
+from fastapi import FastAPI
+
+app = FastAPI()
+
+_import_error = None
+try:
+    from backend.api.main import app as _real_app
+    app = _real_app
+except Exception as e:
+    _import_error = traceback.format_exc()
+
+@app.get("/_debug")
+def debug():
+    return {"sys_path": sys.path, "error": _import_error, "cwd": os.getcwd()}
